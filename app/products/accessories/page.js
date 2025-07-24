711af6037b7a25efaeba04ff2e9f5a394f970946
nakul <jaglan.nakul@gmail.com>
2025-07-24 12:32:20 +0530

    Solved build issues

--- app/products/accessories/page.js
+++ app/products/accessories/page.js
@@ -1,5 +1,5 @@
 "use client";
-import BgLayout from "@/components/templates/bgLayout";
+import BgLayout from "../../../components/templates/bgLayout";
 import { motion } from "framer-motion";
 import { useState } from "react";
 import Image from "next/image";
